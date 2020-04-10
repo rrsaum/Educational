652dc33add67597c4568142bf7dc879e20410399
Rob Saum <61873884+robsaum@users.noreply.github.com>
2020-04-10 15:55:18 -0400

    Update imsmanifest

--- Shared Files/scripts/dmceFunctions.js
+++ Shared Files/scripts/dmceFunctions.js
@@ -698,3 +698,30 @@ function get_params()
 
 }
 
+
+
+function submitFORM(path, params) {
+
+    var form = document.createElement("form");
+    form.setAttribute("method", "POST");
+    form.setAttribute("action", path);
+
+    //Move the submit function to another variable
+    //so that it doesn't get overwritten.
+    form._submit_function_ = form.submit;
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+         }
+    }
+	
+	form.serialize();
+    document.body.appendChild(form);
+    form._submit_function_();
+}
